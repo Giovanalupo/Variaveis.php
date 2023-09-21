0c2cbbc96fe89c899c0d0fcfed2c6289ba084ab0
Giovanalupo <130668804+Giovanalupo@users.noreply.github.com>
2023-09-21 09:00:52 -0300

    Teste variável

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+
+//variável do tipo string 
+$nome = 'Giovana Lupo 123'; 
+echo  "$nome";
+echo "<br>";
+
+//variável do tipo inteiro 
+$ano = 2023; 
+echo "$ano";
+echo "<br>";
+
+//variável do tipo float 
+$pi = 3.14159265; 
+echo "$pi";
+echo "<br>";
+
+//variável do tipo booleano 
+$sim = true ; 
+echo "$sim";
+echo "<br>";
+
+?>
+
+
+
+
+<?php 
+$num = 23.8; 
+if(is_int($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_integer($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_real($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_long($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_float($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+
+<?php 
+$num = 23.8; 
+if(is_string($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_array($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+<?php 
+$num = 23.8; 
+if(is_object($num)) {
+    echo "Número inteiro";
+}
+else {
+    echo "O valor da variável não é um número inteiro!";
+}
+ ?>
+
+
+
+
+
